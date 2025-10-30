9ce757391edfbd000cb87d0735c475aaaf575b67
vlad <vladislav.nikolaenko@p2p.org>
2025-10-30 17:43:13 +0700

    feature: prepared env

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import Widget from '@hexlet/chatbot-v2';
+import steps from '@hexlet/chatbot-v2/example-steps';
 import '@hexlet/chatbot-v2/styles';
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById('root');
+ReactDOM.createRoot(container)
+  .render(Widget(steps));
